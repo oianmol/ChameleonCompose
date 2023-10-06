59fad89648cfdb601a0ef5347d14a261614c883f
Anmol Verma <anmol.verma4@gmail.com>
2023-10-07 01:02:20 +0530

    update: sample

--- app/src/main/java/dev/baseio/chameleon/MainActivity.kt
+++ app/src/main/java/dev/baseio/chameleon/MainActivity.kt
@@ -5,16 +5,12 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
@@ -42,25 +38,18 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(
-    modifier: Modifier = Modifier,
     chameleon: ChangingColorChameleon,
 ) {
     ChameleonUi(
         chameleon = chameleon,
     ) { state ->
-        Scaffold(modifier) {
-            Box(
-                Modifier.fillMaxSize()
-                    .padding(it)
-                    .background(state.color ?: Color.Green),
-            ) {
-                TextButton(onClick = {
+        Box(
+            Modifier.fillMaxSize()
+                .clickable {
                     sendEvent(ChangeColorEvent.ClickPerformed)
-                }, modifier = Modifier.align(Alignment.Center)) {
-                    Text("Change Color")
                 }
-            }
-        }
+                .background(state.color ?: Color.Green),
+        )
     }
 }
 
